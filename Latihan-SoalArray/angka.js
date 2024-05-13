11d2c51041430f6d99a25f26507ae9d217f11a18
Fairuz <firdauz2007123@gmail.com>
2024-05-13 16:38:20 +0700

    first commit

---
+++ Latihan-SoalArray/angka.js
@@ -0,0 +1,11 @@
+// membuat array dalam for menjadi di tambah
+
+let angka = [3,4,5,2,2,3,2] // 3+4+5+6+3+
+let conuter = null // null bisa, cdgak harus angka juga
+
+for(let i = 0; i < angka.length; i++){
+    conuter = conuter + angka[i] 
+    
+}
+console.log(conuter)
+
