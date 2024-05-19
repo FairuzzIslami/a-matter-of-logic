6c8e27c1540c808c466b9896b1e59e69c843749e
Fairuz <firdauz2007123@gmail.com>
2024-05-19 22:04:42 +0700

    learn splice array

---
+++ Learn-ArrayFunction/TaskArray-splice/mengubahNilaibaru.js
@@ -0,0 +1,7 @@
+function namaSiswa(array,star){
+    array = ['fairuz','aqila','islami']
+    star = 2
+    let result = removeAndInsert(array,star)
+    return array,star,result
+}
+console.log(namaSiswa())
